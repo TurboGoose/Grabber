eb80777ef9f7852985356fbbde9dc0d72b3a3ff6
Ilya <antonpuckich@gmail.com>
2020-11-18 13:40:56 +0300

    2.2. Модель данных - Post. [#352067]

---
+++ src/main/java/ru/grabber/Post.java
@@ -0,0 +1,12 @@
+package ru.grabber;
+
+import java.time.LocalDateTime;
+
+public class Post {
+    private int id;
+    private String topic;
+    private String author;
+    private int replies;
+    private int views;
+    private LocalDateTime publication_date;
+}
