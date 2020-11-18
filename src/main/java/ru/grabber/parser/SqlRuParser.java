3b21df427223ac8cd0ebf19c483ef29d4465ce65
Ilya <antonpuckich@gmail.com>
2020-11-18 13:40:36 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#352066]

--- src/main/java/ru/grabber/parser/SqlRuParser.java
+++ src/main/java/ru/grabber/parser/SqlRuParser.java
@@ -4,9 +4,23 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
+import java.io.IOException;
+
 public class SqlRuParser {
-    public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+    public static void main(String[] args) {
+        try {
+            final int NUMBER_OF_PAGES = 5;
+            for (int i = 1; i <= NUMBER_OF_PAGES; i++) {
+                parsePage(i);
+            }
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        }
+    }
+
+    private static void parsePage(int pageNumber) throws IOException {
+        printPageSeparator(pageNumber);
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers/" + pageNumber).get();
         Element table = doc.selectFirst(".forumTable").child(0);
         for (Element row : table.children().subList(4, table.childrenSize())) {
             Element vacancy_name = row.child(1);
@@ -18,5 +32,11 @@ public class SqlRuParser {
 
             System.out.println();
         }
-     }
+    }
+
+    private static void printPageSeparator(int pageNumber) {
+        System.out.printf(
+                "------------------------------------------ Page %d ------------------------------------------%n%n",
+                pageNumber);
+    }
 }
