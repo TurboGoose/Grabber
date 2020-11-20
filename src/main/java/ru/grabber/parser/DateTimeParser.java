2b9031e258b3fd416bdc55be5944290797ad1a6e
Ilya <antonpuckich@gmail.com>
2020-11-20 11:04:00 +0300

    2.1. Преобразование даты [#352070]

--- src/main/java/ru/grabber/parser/DateTimeParser.java
+++ src/main/java/ru/grabber/parser/DateTimeParser.java
@@ -3,9 +3,29 @@ package ru.grabber.parser;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 public class DateTimeParser {
+    private final static Map<String, Integer> MONTHS;
+
+    static {
+        Map<String, Integer> months = new HashMap<>();
+        months.put("янв", 1);
+        months.put("фев", 2);
+        months.put("мар", 3);
+        months.put("апр", 4);
+        months.put("май", 5);
+        months.put("июн", 6);
+        months.put("июл", 7);
+        months.put("авг", 8);
+        months.put("сен", 9);
+        months.put("окт", 10);
+        months.put("ноя", 11);
+        months.put("дек", 12);
+        MONTHS = months;
+    }
+
     public static LocalDateTime parseDateTime(String dateTime) {
         String[] dateAndTime = dateTime.split(", ");
         String date = dateAndTime[0];
@@ -21,13 +41,9 @@ public class DateTimeParser {
     }
 
     private static LocalDate parseDate(String date) {
-        final List<String> months = List.of(
-                "янв", "фев", "мар", "апр",
-                "май", "июн", "июл", "авг",
-                "сен", "окт", "ноя", "дек");
         String[] dayMonthYear = date.split(" ");
         int day = Integer.parseInt(dayMonthYear[0]);
-        int month = months.indexOf(dayMonthYear[1]) + 1;
+        int month = MONTHS.get(dayMonthYear[1]);
         int year = Integer.parseInt(dayMonthYear[2]) + 2000;
         return LocalDate.of(year, month, day);
     }
